4113588a4515e77b57e7fc125548d8ea953f0696
adizbek <adizbek1998@gmail.com>
2020-09-19 17:39:04 +0500

    first commit

---
+++ src/channels.js
@@ -0,0 +1,127 @@
+export class Channel {
+
+  /** @type {string} */
+  url
+
+  /** @typedef {WebSocket} */
+  websocket
+
+  connected = false
+
+  listeners = {}
+
+  reconnectInterval = 5000
+
+  /**
+   * @param {string} url
+   *
+   * @param {object} options
+   * @param {number} options.reconnectInterval
+   *
+   */
+  constructor (url, options = {}) {
+    this.url = url
+    this.connect()
+
+    if (Number.isInteger(options.reconnectInterval) > options.reconnectInterval > 0) {
+      this.reconnectInterval = options.reconnectInterval
+    }
+  }
+
+  connect () {
+    console.log('Trying to connect')
+
+    this.websocket = new WebSocket(this.url)
+
+    this.websocket.onopen = (event) => {
+      console.log('Connection established')
+
+      this._connectionState(true)
+    }
+
+    this.websocket.onclose = (event) => {
+      console.log('Connection closed')
+
+      this._connectionState(false)
+
+      setTimeout(() => {
+        console.log('Reconnecting...')
+
+        this.connect()
+      }, this.reconnectInterval)
+    }
+
+    this.websocket.onmessage = (event) => {
+      let { event: type, payload } = JSON.parse(event.data)
+
+      this.newMessage(type, payload)
+    }
+
+    this.websocket.onerror = (event) => {
+      console.log(event)
+      this.websocket.close()
+    }
+  }
+
+  newMessage (type, payload) {
+    if (Array.isArray(this.listeners[type])) {
+      for (let handler of this.listeners[type]) {
+        handler(payload)
+      }
+    }
+  }
+
+  send (payload) {
+    this.websocket.send(JSON.stringify(payload))
+  }
+
+  /**
+   * @param {string} event
+   * @param {object} payload
+   */
+  sendEvent (event, payload = {}) {
+    this.send({
+      event,
+      payload
+    })
+  }
+
+  $on (eventName, handler) {
+    if (!Array.isArray(this.listeners[eventName])) {
+      this.listeners[eventName] = []
+    }
+
+    this.listeners[eventName].push(handler)
+  }
+
+  $off (eventName, handler) {
+    if (!Array.isArray(this.listeners[eventName])) {
+      this.listeners[eventName] = []
+    }
+
+    this.listeners[eventName] = this.listeners[eventName].filter(x => x !== handler)
+  }
+
+  afterConnect (cb) {
+    if (this.connected === true) {
+      cb()
+    } else {
+      let timer = setInterval(() => {
+        if (this.connected === true) {
+          clearInterval(timer)
+          cb()
+        }
+      }, 50)
+    }
+  }
+
+  _connectionState (state) {
+    this.connected = state
+
+    if (state === true) {
+      this.newMessage('connected')
+    } else {
+      this.newMessage('disconnected')
+    }
+  }
+}
